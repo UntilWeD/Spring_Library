53f3a76dbf07a226edca84202cbac52cb88728f9
Jhin Woo <muojeso90@gmail.com>
2023-07-03 14:53:05 +0900

    ch03

---
+++ SpringMVC_Part_1/servlet/src/main/java/hello/servlet/domain/member/Member.java
@@ -0,0 +1,20 @@
+package hello.servlet.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Member {
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member(){
+    }
+}
